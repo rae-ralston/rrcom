aa4b9abbf75f3bba2d42c2fb6a063a192fe0b865
Rachel Ralston <rachel.ralston@kofile.com>
2022-08-15 12:57:31 -0700

    chore: remove unused files

--- src/components/portfolio.card.js
+++
@@ -1,8 +0,0 @@
-import { Card } from "./styles/_portfolio.card";
-
-export const ClientCard = ({ companyName, images, blurb }) => (
-  <Card>
-    <img src={images[0]} alt={`rae ralston portfolio for ${companyName}`} />
-    <h3>{companyName}</h3>
-  </Card>
-);
