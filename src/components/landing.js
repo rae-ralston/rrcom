148c903fc4b929fe3fdcdc2697491db91bfb41e9
Rachel Ralston <rachel.ralston@kofile.com>
2022-08-25 22:18:01 -0400

    feat: p5js hello world

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,12 +1,17 @@
+import sketch from "../sketch";
 import Art from "./landing.card.art";
 import Code from "./landing.card.code";
 import Container from "./styles/landing";
+import { ReactP5Wrapper } from "react-p5-wrapper";
 
 const Landing = () => (
-  <Container>
-    <Art />
-    <Code />
-  </Container>
+  <>
+    <ReactP5Wrapper sketch={sketch} />
+    <Container>
+      <Art />
+      <Code />
+    </Container>
+  </>
 );
 
 export default Landing;
