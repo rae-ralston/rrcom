e36b78538558953f71ffc01da2b147b8e36418f0
Rae Ralston <rachel.ralston@kofile.com>
2022-10-18 13:36:14 -0500

    Add three js (#3)
    * add three js
    
    * feat: init display three

---
+++ src/sketches/221018.js
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+import { useEffect } from "react";
+
+export const getData = (o) => Object.values(o);
+
+const Three = ({ id, height, width }) => {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const fov = 75;
+    const aspectRatio = width / height;
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshNormalMaterial();
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const camera = new THREE.PerspectiveCamera(fov, aspectRatio, 0.1, 100);
+    camera.position.z = 3;
+    scene.add(camera);
+
+    const canvas = document.getElementById(id);
+
+    const renderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    const clock = new THREE.Clock();
+
+    const elapsedTime = clock.getElapsedTime();
+
+    const animate = () => {
+      mesh.rotation.y = elapsedTime;
+      mesh.rotation.x = elapsedTime;
+
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, [height, width]);
+
+  return <canvas id={id} />;
+};
+
+export default Three;
